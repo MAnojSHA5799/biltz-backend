005f671dfe772d8a1f4eec2bb1f5e2c0e8185e25
MAnojSHA5799 <manojshakya54@gmail.com>
2024-02-27 15:26:16 +0530

    leave

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 import express from "express";
-import cors from 'cors';
-import pkg from 'pg';
+import cors from "cors";
+import pkg from "pg";
 import Jwt from "jsonwebtoken";
-import bcrypt from 'bcrypt'
+import bcrypt from "bcrypt";
 import multer from "multer";
 import path from "path";
 import cookieParser from "cookie-parser";
-import mysql from 'mysql2';
+import mysql from "mysql2";
 const { Client } = pkg;
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static('Public'))
+app.use(express.static("Public"));
 app.use(cors());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -26,9 +26,6 @@ app.use(function (req, res, next) {
   );
   next();
 });
-
-
-
 
 // const client = new Client({
 //   user: "postgres",
@@ -47,121 +44,115 @@ app.use(function (req, res, next) {
 //     console.error("Connection error:", error);
 //   });
 
+// let client = mysql.createConnection({
+//   host: "localhost",
+//   port: "3306",
+//   user: "newsite",
+//   password: "Alkmaar@123",
+//   database: "newsite2020",
+// });
 
-  // let client = mysql.createConnection({
-  //   host: "localhost",
-  //   port: "3306",
-  //   user: "newsite",
-  //   password: "Alkmaar@123",
-  //   database: "newsite2020",
-  // });
-  
-  // client.connect((err) => {
-  //   if (err) return console.error(err.message);
-  
-  //   console.log('Connected to the MySQL server.');
-  // });
+// client.connect((err) => {
+//   if (err) return console.error(err.message);
 
-  const client = new Client({
-    user: "postgres",
-    password: "Biltz123@990",
-    database: "postgres",
-    port: 5432,
-    host: "db.adbusyzbvzlgetciiwso.supabase.co",
-    ssl: { rejectUnauthorized: false },
+//   console.log('Connected to the MySQL server.');
+// });
+
+const client = new Client({
+  user: "postgres",
+  password: "Biltz123@990",
+  database: "postgres",
+  port: 5432,
+  host: "db.adbusyzbvzlgetciiwso.supabase.co",
+  ssl: { rejectUnauthorized: false },
+});
+
+client
+  .connect()
+  .then(() => {
+    console.log("Connected!!!");
+  })
+  .catch((error) => {
+    console.error("Connection error:", error);
   });
 
-  client.connect()
-    .then(() => {
-      console.log("Connected!!!");
-    })
-    .catch((error) => {
-      console.error("Connection error:", error);
-    });
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
 
-    app.get("/", (req, res) => {
-      res.send("Hello, World!");
-    });
-
-
-  app.post("/addDetails", async (req, res) => {
-    console.log(req.body)
-              const sql = `INSERT INTO usermessages 
+app.post("/addDetails", async (req, res) => {
+  console.log(req.body);
+  const sql = `INSERT INTO usermessages 
                   (username, email, phone, subject, message) 
                   VALUES ($1, $2, $3, $4, $5)`;
-              const values = [
-                  req.body.username,
-                  req.body.email,
-                  req.body.phone,
-                  req.body.subject,
-                  req.body.message,
-              ];
-      
-              client.query(sql, values, (err, result) => {
-                if (err) {
-                  res.status(500).send(err);
-                } else {
-                  res.status(201).json({ message: 'User details inserted successfully' });
-                }
-              });
-      });
+  const values = [
+    req.body.username,
+    req.body.email,
+    req.body.phone,
+    req.body.subject,
+    req.body.message,
+  ];
 
+  client.query(sql, values, (err, result) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).json({ message: "User details inserted successfully" });
+    }
+  });
+});
 
-      app.post("/consultationQuery", async (req, res) => {
-        console.log(req.body)
-                  const sql = `INSERT INTO userConsultQuery 
+app.post("/consultationQuery", async (req, res) => {
+  console.log(req.body);
+  const sql = `INSERT INTO userConsultQuery 
                       (name, email, phone, selected_option) 
                       VALUES ($1, $2, $3, $4)`;
-                  const values = [
-                      req.body.name,
-                      req.body.email,
-                      req.body.phone,
-                      req.body.selectedOption,
-                  ];
-          
-                  client.query(sql, values, (err, result) => {
-                    if (err) {
-                      res.status(500).send(err);
-                    } else {
-                      res.status(201).json({ message: 'User details inserted successfully' });
-                    }
-                  });
-          });
-          app.post("/blogComments", async (req, res) => {
-            console.log(req.body)
-                      const sql = `INSERT INTO comments 
+  const values = [
+    req.body.name,
+    req.body.email,
+    req.body.phone,
+    req.body.selectedOption,
+  ];
+
+  client.query(sql, values, (err, result) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).json({ message: "User details inserted successfully" });
+    }
+  });
+});
+app.post("/blogComments", async (req, res) => {
+  console.log(req.body);
+  const sql = `INSERT INTO comments 
                           (name, email, message) 
                           VALUES ($1, $2, $3)`;
-                      const values = [
-                          req.body.name,
-                          req.body.email,
-                          req.body.message 
-                      ];
-              
-                      client.query(sql, values, (err, result) => {
-                        if (err) {
-                          res.status(500).send(err);
-                        } else {
-                          res.status(201).json({ message: 'User details inserted successfully' });
-                        }
-                      });
-              });
-              app.get("/allusers", function (req, res) {
-                let query = `SELECT * FROM userconsultquery`;
-              
-                client.query(query, (err, result) => {
-                  console.log("140");
-                  if (err) {
-                    console.error("Error executing query:", err); // Log the error to console
-                    console.error("Query:", query); // Log the query that caused the error
-                    res.status(500).json({ error: "Internal Server Error" }); // Send a more informative response
-                  } else {
-                    console.log("Query result:", result.rows);
-                     res.json(result.rows);
-                    // res.json(result);
-                  }
-                });
-              });
+  const values = [req.body.name, req.body.email, req.body.message];
+
+  client.query(sql, values, (err, result) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).json({ message: "User details inserted successfully" });
+    }
+  });
+});
+app.get("/allusers", function (req, res) {
+  let query = `SELECT * FROM userconsultquery`;
+
+  client.query(query, (err, result) => {
+    console.log("140");
+    if (err) {
+      console.error("Error executing query:", err); // Log the error to console
+      console.error("Query:", query); // Log the query that caused the error
+      res.status(500).json({ error: "Internal Server Error" }); // Send a more informative response
+    } else {
+      console.log("Query result:", result.rows);
+      res.json(result.rows);
+      // res.json(result);
+    }
+  });
+});
 
 //   const verifyUser = (req, res, next) => {
 //     const token = req.cookies.token;
@@ -208,29 +199,29 @@ app.use(function (req, res, next) {
 // });
 
 app.get("/allusers1", function (req, res) {
-    let query = "SELECT * FROM admin";
-    client.query(query, (err, result) => {
-      if (err) {
-        res.status(500).send(err);
-      } else {
-        res.send(result.rows);
-      }
-    });
+  let query = "SELECT * FROM admin";
+  client.query(query, (err, result) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.send(result.rows);
+    }
   });
-  // ,upload.single('image')
-  app.get("/employee", function (req, res) {
-    let query = "SELECT * FROM employee where role='user'";
-    client.query(query, (err, result) => {
-      if (err) {
-        res.status(500).send(err);
-      } else {
-        res.send(result.rows);
-      }
-    });
+});
+// ,upload.single('image')
+app.get("/employee", function (req, res) {
+  let query = "SELECT * FROM employee where role='user'";
+  client.query(query, (err, result) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.send(result.rows);
+    }
   });
+});
 //   app.post("/addemployee", async (req, res) => {
-//           const sql = `INSERT INTO employee 
-//               (name, photo, designation, email, password, date_of_joining, manager) 
+//           const sql = `INSERT INTO employee
+//               (name, photo, designation, email, password, date_of_joining, manager)
 //               VALUES ($1, $2, $3, $4, $5, $6, $7)`;
 //           const values = [
 //               req.body.name,
@@ -241,7 +232,7 @@ app.get("/allusers1", function (req, res) {
 //               req.body.dateOfJoining,
 //               req.body.manager,
 //           ];
-  
+
 //           client.query(sql, values, (err, result) => {
 //             if (err) {
 //               res.status(500).send(err);
@@ -261,8 +252,8 @@ app.get("/allusers1", function (req, res) {
 
 // app.put('/edit_employee/:id', (req, res) => {
 //     const id = req.params.id;
-//     const sql = `UPDATE employee 
-//         set name = $1, designation = $2, email = $3, password =$4, date_of_joining = $5, manager = $6 
+//     const sql = `UPDATE employee
+//         set name = $1, designation = $2, email = $3, password =$4, date_of_joining = $5, manager = $6
 //         Where id = $7`
 //     const values = [
 //         req.body.name,
@@ -278,7 +269,6 @@ app.get("/allusers1", function (req, res) {
 //     })
 // })
 
-
 //   app.delete('/deleteemployee/:id', (req, res) => {
 //     const id = req.params.id;
 //     const sql = "delete from employee where id = $1"
@@ -290,7 +280,7 @@ app.get("/allusers1", function (req, res) {
 
 // app.post("/leaveapply", async (req, res) => {
 //   const sql = `INSERT INTO leavedata
-//       (name, designation, startDate, endDate, reason, status, leaves_days) 
+//       (name, designation, startDate, endDate, reason, status, leaves_days)
 //       VALUES ($1, $2, $3, $4, $5, $6, $7)`;
 
 //   const values = [
@@ -343,7 +333,7 @@ app.get("/allusers1", function (req, res) {
 
 // app.put('/edit_leave_form/:id', (req, res) => {
 //   const id = req.params.id;
-//   const sql = `UPDATE leavedata 
+//   const sql = `UPDATE leavedata
 //       set name = $1, designation = $2, startdate = $3, enddate = $4, reason = $5, status = $6, leaves_days= $7
 //       Where id = $8`
 //   const values = [
@@ -364,12 +354,12 @@ app.get("/allusers1", function (req, res) {
 
 // app.put('/leave_reject/:id', (req, res) => {
 //   const id = req.params.id;
-//   const sql = `UPDATE leavedata 
+//   const sql = `UPDATE leavedata
 //       set status = $1
 //       Where id = $2`
 //   const values = [req.body.data.status]
 //   client.query(sql,[...values, id], (err, result) => {
-//       if(err) 
+//       if(err)
 //       return res.json({Status: true, Result: result})
 //        return res.json({Status: false, Error: "Query Error"+err})
 //   })
@@ -377,19 +367,16 @@ app.get("/allusers1", function (req, res) {
 
 // app.put('/leave_approve/:id', (req, res) => {
 //   const id = req.params.id;
-//   const sql = `UPDATE leavedata 
+//   const sql = `UPDATE leavedata
 //       set status = $1
 //       Where id = $2`
 //   const values = [req.body.data.status]
 //   client.query(sql,[...values, id], (err, result) => {
-//       if(err) 
+//       if(err)
 //       return res.json({Status: true, Result: result})
 //        return res.json({Status: false, Error: "Query Error"+err})
 //   })
 // })
 
-
-  
-   
 const port = process.env.PORT || 2410;
 app.listen(port, () => console.log(`Node app listening on port ${port}!`));
